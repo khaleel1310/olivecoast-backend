f4e63769fffb994672004101a8ce7a11bf306293
Khaleel AL-Gohany <khalilalrefae13@gmail.com>
2026-08-24 03:14:20 +0300

    changed prisma client on bookings.controller.ts

--- src/controllers/bookings.controller.ts
+++ src/controllers/bookings.controller.ts
@@ -1,6 +1,5 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../prisma/client';
 
-const prisma = new PrismaClient();
 
 export const createBooking = async (data: any) => {
   const { 
